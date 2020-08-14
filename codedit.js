3597f4469692d28bc1b90df4ae14ae11f9047483
Bulagro <christumer@gmail.com>
2020-08-14 16:22:12 -0500

    Pyodide funcional en la página.

--- codedit.js
+++ codedit.js
@@ -1,3 +1,11 @@
+// Carga python y todas las librerías que necesitamos.
 languagePluginLoader.then(() => {
     console.log(pyodide.runPython('import sys\nsys.version'));
+    pyodide.loadPackage(['pandas', 'matplotlib']);
 });
+
+// Ejecuta el código
+function run() {
+    var code = editor.doc.getValue();
+    pyodide.runPython(code);
+}
