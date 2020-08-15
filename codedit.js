38b084a787ee0ee5d06b65acebb463af2f2fa4bc
Bulagro <christumer@gmail.com>
2020-08-15 12:55:33 -0500

    Implementar funciones de setcode.js en codedit.js

--- codedit.js
+++ codedit.js
@@ -37,3 +37,23 @@ function run() {
 
     graficas.scrollIntoView();
 }
+
+function resetCode() {
+    editor.setValue(`import matplotlib.pyplot as plt
+import io, base64
+
+GRAFICAS = []
+
+for i in ([1, 2, 3], [2, 5, -3], [5, 10, -2, -20]):
+    fig, ax = plt.subplots()
+    ax.plot(i)
+
+    buf = io.BytesIO()
+    fig.savefig(buf, format='png')
+    buf.seek(0)
+    GRAFICAS.append('data:image/png;base64,' + base64.b64encode(buf.read()).decode('UTF-8'))
+
+`);
+}
+
+resetCode();
