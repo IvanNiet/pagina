5174188184a8f3854a38694cab59dad658c93e15
Bulagro <christumer@gmail.com>
2020-08-14 19:21:32 -0500

    Mensajes varios.
    - Al hacer click en el botón de ejecución.
    - Al cargar el código.

--- codedit.js
+++ codedit.js
@@ -2,12 +2,13 @@ var editor;
 
 // Carga python y todas las librerías que necesitamos.
 languagePluginLoader.then(() => {
-    console.log(pyodide.runPython('import sys\nsys.version'));
+    // console.log(pyodide.runPython('import sys\nsys.version')); // Python 3.7.4
+
     pyodide.loadPackage(['pandas', 'matplotlib', 'numpy']).then(() => {
         editor = CodeMirror.fromTextArea(document.getElementById('code'), {
             mode: {name: "python",
-                version: 3,
-                singleLineStringErrors: false},
+            version: 3,
+                singleLineStrinxgErrors: false},
             lineNumbers: true,
             indentUnit: 4,
             matchBrackets: true
@@ -15,16 +16,17 @@ languagePluginLoader.then(() => {
         editor.setSize(null, "20em");
 
         document.getElementById('espacio-de-codigo').style.visibility = 'visible';
+        document.getElementById('h4-codigo').innerHTML = 'Código';
     });
+
 });
 
 // Ejecuta el código
 function run() {
-    var code = editor.doc.getValue();
-    pyodide.runPython(code);
+    pyodide.runPython(editor.doc.getValue());
+    document.getElementById('mensaje-ejecutando').innerHTML = 'Listo!';
 
     grafica = document.getElementById('grafica');
-
     grafica.style.visibility = 'visible';
     grafica.src=pyodide.globals.img_str;
 
