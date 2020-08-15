410861a0ab1f570ee3d9cb10da63a98bbe8ad042
Bulagro <christumer@gmail.com>
2020-08-15 12:57:49 -0500

    Eliminación de la creación del editor de codedit.js

--- codedit.js
+++ codedit.js
@@ -1,28 +1,3 @@
-var editor;
-
-// Carga python y todas las librerías que necesitamos.
-languagePluginLoader.then(() => {
-    // console.log(pyodide.runPython('import sys\nsys.version')); // Python 3.7.4
-
-    pyodide.loadPackage(['pandas', 'matplotlib', 'numpy']).then(() => {
-        editor = CodeMirror.fromTextArea(document.getElementById('code'), {
-            mode: {
-                name: "python",
-            version: 3,
-                singleLineStrinxgErrors: false
-            },
-            lineNumbers: true,
-            indentUnit: 4,
-            matchBrackets: true
-        });
-        editor.setSize(null, "20em");
-
-        document.getElementById('espacio-de-codigo').style.visibility = 'visible';
-        document.getElementById('h4-codigo').innerHTML = 'Código';
-    });
-
-});
-
 // Ejecuta el código
 function runCode() {
     pyodide.runPython(editor.doc.getValue());
